0dfa81ceddbdedb83a6a7b0610573cab2d364604
AsadSasoli <asadsasoli8@gmail.com>
2023-06-12 00:38:10 +0500

    first commit

---
+++ api.js
@@ -0,0 +1,65 @@
+var project =[
+    {
+        id:1,
+        name:"Burgar",
+        cetagry:"denner",
+        image:"IMG/1.jpeg",
+        button:"Get Oder VIP Burgur",
+    },
+    {
+        id:2,
+        name:" Peatha",
+        cetagry:"nashta",
+        image:"IMG/2.png",
+        button:"Get Oder VIP Pratha"
+    },
+    {
+        id:3,
+        name:"chai",
+        cetagry:"nashta",
+        image:"IMG/2.png",
+        button:"Get Oder VIP Pratha"
+    },
+    {
+        id:4,
+        name:"Pai",
+        cetagry:"nashta",
+        image:"IMG/2.png",
+        button:"Get Oder VIP Pratha"
+    },
+    {
+        id:5,
+        name:"Aalo ka pratha",
+        cetagry:"nashta",
+        image:"IMG/2.png",
+        button:"Get Oder VIP Pratha"
+    },
+    {
+        id:6,
+        name:"papa",
+        cetagry:"nashta",
+        image:"IMG/2.png",
+        button:"Get Oder VIP Pratha"
+    },
+    {
+        id:7,
+        name:"salen",
+        cetagry:"nashta",
+        image:"IMG/2.png",
+        button:"Get Oder VIP Pratha"
+    },
+    {
+        id:8,
+        name:" Peatha",
+        cetagry:"nashta",
+        image:"IMG/2.png",
+        button:"Get Oder VIP Pratha"
+    },
+    {
+        id:9,
+        name:"daal",
+        cetagry:"lunch",
+        image:"IMG/3.png",
+        button:"Get Oder VIP Daal"
+    },
+]
